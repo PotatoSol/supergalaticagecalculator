98432e6599fcd77d0864e711a37c3fe436fe952d
PotatoSol <sol.mike.j@gmail.com>
2023-02-03 09:47:26 -0800

    catch if nothing is input, wip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {User} from "./../src/js/calculator.js";
 //import {Game} from "./../src/js/game.js";
 function main(input){
-  console.log(input);
+  if(input === ""){
+    console.log('what');
+  }
 }
 
 window.addEventListener("load", function(){
-  console.log('hehe')
   document.getElementById("ageForm").addEventListener("submit",function(event){
     event.preventDefault();
     main(parseInt(document.getElementById("age").value));
