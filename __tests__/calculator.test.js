eb59318ab78b32b8d5708e8a43383017b8ddc4c0
PotatoSol <sol.mike.j@gmail.com>
2023-02-03 08:24:51 -0800

    good fail for declaration of object, renamed calculator.js to be correct

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,8 +1,8 @@
-import {User} from "../src/js/calculator.js/";
+import {User} from "./../src/js/calculator.js";
 
 describe ('User', () => {
 });
 test('Declare a User Object', () =>{
-  const myUser = new User();
-
+  const myUser = new User(1, 1, 1990);
+  expect(!myUser).toBeTruthy();
 });
---
+++ src/js/calculator.js
@@ -0,0 +1,11 @@
+//create a user object
+// - take in their birthdate
+
+
+export class User{
+  constructor(month, day, year){
+    this.month = month;
+    this.day = day;
+    this.year = year;
+  }
+}
