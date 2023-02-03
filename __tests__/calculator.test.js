e01afac55c12fe2aa21a5c28c8207028117194f1
PotatoSol <sol.mike.j@gmail.com>
2023-02-03 12:58:41 -0800

    forgot to write out tests first for setting age

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -3,18 +3,26 @@ import {User} from "./../src/js/calculator.js";
 describe ('User', () => {
 });
 test('Declare a User Object', () =>{
-  const myUser = new User(30);
+  const myUser = new User();
   expect(myUser).toBeTruthy();
 });
+test('setAge()', () => {
+  const myUser = new User();
+  myUser.setAge(30);
+  expect(myUser.age).toEqual(20);
+});
 test('calculateAge()', () => {
-  const myUser = new User(30);
+  const myUser = new User();
+  myUser.setAge(30);
   expect(myUser.calculateAge(.5)).toEqual(60);
 });
 test('calculateYearsSince()', () => {
-  const myUser = new User(30);
+  const myUser = new User();
+  myUser.setAge(30);
   expect(myUser.calculateYearsSince(10)).toEqual(20);
 });
 test('calculateYearsUntil()', () => {
-  const myUser = new User(30);
+  const myUser = new User();
+  myUser.setAge(30);
   expect(myUser.calculateYearsUntil(40)).toEqual(10);
 });
