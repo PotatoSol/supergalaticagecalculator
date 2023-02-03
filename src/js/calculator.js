0f56be9a1330896b44689683850e28d3d1c7e1ef
PotatoSol <sol.mike.j@gmail.com>
2023-02-03 08:37:58 -0800

    added some constants for later use

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -5,6 +5,10 @@
 export class User{
   constructor(age){
     this.age = age;
+    const MERCURY = .24;
+    const VENUS = .62;
+    const MARS = 1.88;
+    const JUPITER = 11.86;
   }
 
   calculateAge(yearLength){ //yearLength as a decimal of earth years
