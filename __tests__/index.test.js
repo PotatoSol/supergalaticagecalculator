e5084f2e103118ce26ac7076ca6d554e0789ba62
PotatoSol <sol.mike.j@gmail.com>
2023-02-05 12:41:59 -0800

    pass for doCalculation)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,8 +6,8 @@ describe ('doCalculation', () => {
 });
 test('doCalculation', () =>{
   const myUser = new User();
-  myUser.setAge(10);
+  myUser.setAge(24);
   myUser.setFunctionChoice("convert");
   myUser.setPlanet("mercury");
-  expect(doCalculation(myUser)).toEqual(24);
+  expect(doCalculation(myUser)).toEqual(100);
 });
