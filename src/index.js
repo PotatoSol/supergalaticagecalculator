49733437bf0eb5a0b571a1c4a306cc6aac89bbe1
PotatoSol <sol.mike.j@gmail.com>
2023-02-05 10:55:41 -0800

    more ui work

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import {User} from "./../src/js/calculator.js";
-import './css/styles.css'
+import './css/styles.css';
 
 function handleAgeSubmit(input, inputUser){
   if(input.length === 0){
     //catch that nothing was put in, and then yell at the user
-    console.log('hello');
-    //document.getElementById('ageError').setAttribute('class', "notHidden");
+    showErrorMessage(1);
   }else if(!parseInt(input)){
     //catch if no int was put in?
-    console.log('wow!');
+    showErrorMessage(2);
   }else{
+    showErrorMessage(3);
     inputUser.setAge(input);
   }
 }
@@ -20,6 +20,36 @@ function handlePlanetSubmit(input, inputUser){
 
 function handleFunctionChoiceSubmit(input, inputUser){
   inputUser.setFunctionChoice(input);
+}
+
+function checkInputs(user){
+  if(user.age === -1 || user.planet === "void" || user.functionChoice === "void"){
+    return false;
+  } else {
+    doCalculation(user);
+  }
+}
+
+function doCalculation(user){
+  if(user.function === "convert"){
+    //do convert
+  } else if (user.function === "since"){
+    //do since
+  } else if (user.function === "until"){
+    //do until
+  } else {
+    //should never happen, throw error message?
+  }
+}
+
+function showErrorMessage(value){
+  if(value === 1){
+    document.getElementById('ageError').innerText = "Nothing entered, please input your age";
+  } else if (value === 2){
+    document.getElementById('ageError').innerText = "Non-number entered, please input your age";
+  } else { 
+    document.getElementById('ageError').innerText = "";
+  }
 }
 
 window.addEventListener("load", function(){
