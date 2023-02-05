70d152b9fae3b49b0aa4cf2cb8b2e6e9afad1e86
PotatoSol <sol.mike.j@gmail.com>
2023-02-05 12:52:16 -0800

    good fail for cehckInputs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import {User} from "./../src/js/calculator.js";
-import {doCalculation} from "./../src/index.js";
+import {doCalculation, checkInputs} from "./../src/index.js";
 import "./../src/css/styles.css";
 
 describe ('doCalculation', () => {
@@ -10,4 +10,13 @@ test('doCalculation', () =>{
   myUser.setFunctionChoice("convert");
   myUser.setPlanet("mercury");
   expect(doCalculation(myUser)).toEqual(100);
+});
+describe ('checkInputs', () => {
+});
+test('checkInputs', () =>{
+  const myUser = new User();
+  myUser.setAge(24);
+  myUser.setFunctionChoice("convert");
+  myUser.setPlanet("mercury");
+  expect(checkInputs(myUser)).toEqual(false);
 });
