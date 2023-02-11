c1e54e5d5bc7f60fa3db1a598b874abdeb74a4a2
PotatoSol <sol.mike.j@gmail.com>
2023-02-11 13:41:31 -0800

    still cleaning code

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -13,55 +13,46 @@ test('setAge()', () => {
 });
 test('calculateAge()', () => {
   const myUser = new User();
-  myUser.setAge(30);
+  myUser.age = 30;
   expect(myUser.calculateAge(.5)).toEqual(60);
 });
 test('calculateYearsSince()', () => {
   const myUser = new User();
-  myUser.setAge(30);
+  myUser.age = 30;
   expect(myUser.calculateYearsSince(10)).toEqual(20);
 });
 test('calculateYearsUntil()', () => {
   const myUser = new User();
-  myUser.setAge(30);
+  myUser.age = 30;
   expect(myUser.calculateYearsUntil(40)).toEqual(10);
 });
 test('setPlanet()', () => {
   const myUser = new User();
-  myUser.setPlanet("mars");
+  myUser.planet = "mars"
   expect(myUser.planet).toEqual("mars");
 });
 test('calculatePlanetNumber)', () => {
   const myUser = new User();
   myUser.calculatePlanetNumber();
   expect(myUser.planetNumber).toEqual(-1);
-  myUser.setPlanet("mercury");
+  myUser.planet = "mercury"
   expect(myUser.planetNumber).toEqual(.24);
-  myUser.setPlanet("venus");
-  expect(myUser.planetNumber).toEqual(.62);
-  myUser.setPlanet("mars");
-  expect(myUser.planetNumber).toEqual(1.88);
-  myUser.setPlanet("jupiter");
-  expect(myUser.planetNumber).toEqual(11.86);
 });
 test('setFunctionChoice()', () => {
   const myUser = new User();
-  myUser.setFunctionChoice("until");
+  myUser.functionChoice("until");
   expect(myUser.functionChoice).toEqual("until");
 });
 test('moreYears()', () => {
   const myUser = new User();
-  myUser.setMoreYears(10);
+  myUser.moreYears = 10;
   expect(myUser.moreYears).toEqual(10);
 });
 test('doCalculation()', () => {
   const myUser = new User();
-  myUser.setFunctionChoice('convert');
-  myUser.setAge('20');
-  myUser.setPlanet('mercury');
+  myUser.functionChoice = "convert";
+  myUser.age = 20;
+  myUser.planet = "mercury";
   expect(myUser.doCalculation()).toEqual(83.33333333333334);
-
-});
-test('showResults()', () => {
 
 });
